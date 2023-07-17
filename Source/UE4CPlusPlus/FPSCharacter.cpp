e654680f9909edc6739b4bd663d63c7369a1b068
XYCDWB <1248482494@qq.com>
2023-07-17 17:41:34 +0800

    20230717

--- Source/UE4CPlusPlus/FPSCharacter.cpp
+++ Source/UE4CPlusPlus/FPSCharacter.cpp
@@ -9,6 +9,39 @@ AFPSCharacter::AFPSCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//创建第一人称摄像机组件
+	FPSCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	check(FPSCameraComponent != nullptr);
+
+	//将摄像机组件附加到胶囊体组件
+	//创建 UCameraComponent，并将其附加到角色的 CapsuleComponent
+	FPSCameraComponent->SetupAttachment(CastChecked<USceneComponent,UCapsuleComponent>(GetCapsuleComponent()));
+
+	//将摄像机置于略高于眼睛上方的位置
+	FPSCameraComponent->SetRelativeLocation(FVector(0.0f,0.0f,50.0f+BaseEyeHeight));
+
+	//启用Pawn控制摄像机旋转
+	FPSCameraComponent->bUsePawnControlRotation = true;
+
+	//为所属玩家创建第一人称网格体组件
+	FPSMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FirstPersonMesh"));
+	check(FPSMesh != nullptr);
+
+	//SetOnlyOwnerSee 表示此网格体仅对拥有此角色的 PlayerController 可见。
+	//此代码还将网格体附加到摄像机，并禁用某些环境阴影。让手臂投射阴影会破坏第一人称角色应该只有单个网格体的感觉
+	//仅所属玩家可以看见此网格体
+	FPSMesh->SetOnlyOwnerSee(true);
+
+	//将FPS网格体附加到FPS摄像机
+	FPSMesh->SetupAttachment(FPSCameraComponent);
+
+	//禁用某些环境阴影以便实现只有单个网格体的感觉
+	FPSMesh->bCastDynamicShadow = false;
+	FPSMesh->CastShadow = false;
+
+	//所属玩家看不到常规(第三人称)全身网格体
+	GetMesh()->SetOwnerNoSee(true);
+
 }
 
 // Called when the game starts or when spawned
@@ -18,9 +51,12 @@ void AFPSCharacter::BeginPlay()
 
 	check(GEngine != nullptr);
 
-	// 显示调试消息五秒。 
-	// -1"键"值参数可以防止更新或刷新消息。
-	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("We are using FPSCharacter."));
+	if (GEngine)
+	{
+		// 显示调试消息五秒。 
+		// -1"键"值参数可以防止更新或刷新消息。
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("We are using FPSCharacter."));
+	}
 	
 }
 
@@ -47,6 +83,9 @@ void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 	//设置跳跃绑定
 	PlayerInputComponent->BindAction("Jump", IE_Pressed,this,&AFPSCharacter::StartJump);
 	PlayerInputComponent->BindAction("Jump",IE_Released,this,&AFPSCharacter::StopJump);
+
+	//绑定开火
+	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AFPSCharacter::Fire);
 }
 
 void AFPSCharacter::MoveForWard(float AxisValue)
@@ -71,5 +110,46 @@ void AFPSCharacter::StartJump()
 void AFPSCharacter::StopJump()
 {
 	bPressedJump = false;
+}
+
+void AFPSCharacter::Fire()
+{
+	//试图发射发射物
+	if (ProjectileClass)
+	{
+		//获取摄像机变换
+		FVector CameraLocation;
+		FRotator CameraRotation;
+		GetActorEyesViewPoint(CameraLocation, CameraRotation);
+		
+		//设置MuzzOffset，在略靠近摄像机前生成发射物
+		MuzzleOffset.Set(100.0f,0.0f,0.0f);
+
+		//将MuzzleOffset从摄像机空间变换到世界空间
+		FVector MuzzleLocation = CameraLocation + FTransform(CameraRotation).TransformVector(MuzzleOffset);
+
+		//使目标方向略向上倾斜
+		FRotator MuzzleRotation = CameraRotation;
+		MuzzleRotation.Pitch += 10.0f;
+
+		UWorld * world = GetWorld();
+		if (world)
+		{
+			FActorSpawnParameters SpawnParams;
+			SpawnParams.Owner = this;
+			SpawnParams.Instigator = GetInstigator();
+
+			//在枪口的位置生成发射物
+			AFPSProjectile* Projectile = world->SpawnActor<AFPSProjectile>(ProjectileClass, MuzzleLocation, MuzzleRotation, SpawnParams);
+			if (Projectile)
+			{
+				//设置发射物的初始轨迹
+				FVector LaunchDirection = MuzzleRotation.Vector();
+				Projectile->FireInDirection(LaunchDirection);
+			}
+		}
+	}
+
+
 }
 
--- Source/UE4CPlusPlus/FPSCharacter.h
+++ Source/UE4CPlusPlus/FPSCharacter.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "FPSProjectile.h"
 #include "FPSCharacter.generated.h"
 
 UCLASS()
@@ -42,4 +45,33 @@ public:
 	UFUNCTION()
 	void StopJump();
 
+	//FPS摄像机
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* FPSCameraComponent;
+
+	//第一人称网格体(手臂)，仅对所属玩家可见
+	UPROPERTY(VisibleDefaultsOnly,Category = Mesh)
+	USkeletalMeshComponent* FPSMesh;
+
+	//处理发射物射击的函数
+	void Fire();
+
+
+	//生成 FPSProjectile actor并实现 OnFire 函数时需要考虑两点，即：
+
+	//发射物对应的类（让 FPSCharacter 及其派生蓝图知道要生成哪种发射物）。
+
+	//将使用一个摄像机空间中的偏移向量来确定发射物的生成位置。
+	//设置该参数为可编辑参数，这样可以在 BP_FPSCharacter 蓝图中对其进行设置和调整。
+	//可以基于这些数据计算发射物的初始位置。
+	//枪口相对于摄像机位置的偏移
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = Gameplay)
+	FVector MuzzleOffset;
+
+	//要生成的发射物类
+	UPROPERTY(EditDefaultsOnly,Category = Projectile)
+	TSubclassOf<class AFPSProjectile> ProjectileClass;
+
 };
+
+
